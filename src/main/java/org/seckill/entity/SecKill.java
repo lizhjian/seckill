26bfff1d6c323efab5af67e68a0ad6e18a1f9495
lizj2 <907257608@qq.com>
2020-03-29 07:28:50 +0800

    建包,创建实体

---
+++ src/main/java/org/seckill/entity/SecKill.java
@@ -0,0 +1,33 @@
+package org.seckill.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * <pre>
+ * desc ：
+ * author ：lizj
+ * date ：2020-03-29 07:18
+ * </pre>
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SecKill {
+
+    private long seckillId;
+
+    private String name;
+
+    private Integer number;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private Date createTime;
+
+}
---
+++ src/main/java/org/seckill/entity/SeuccessKilled.java
@@ -0,0 +1,27 @@
+package org.seckill.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * <pre>
+ * desc ：秒杀成功类
+ * author ：lizj
+ * date ：2020-03-29 07:22
+ * </pre>
+ */
+@Data
+public class SeuccessKilled {
+
+    private Long seckillId;
+
+    private Long userPhone;
+
+    private Integer state;
+
+    private Date createTime;
+
+    private SecKill secKill;
+
+}
